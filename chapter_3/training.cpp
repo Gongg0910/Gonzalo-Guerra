5fa399ba5c89fcc0d20d3eb3429f6fccee653d4d
Gongg0910 <chefgonzalonyc@gmail.com>
2025-09-21 22:46:34 -0400

    playing :D

---
+++ chapter_3/training.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+/* int main(){
+    string word;
+    while(cin >> word){
+        
+    }
+    return 0;
+} */
+
+
+/* int main(){
+    string line;
+    while(getline(cin, line)){
+        
+    }
+
+    return 0;
+} */
+
+
+/* int main(){
+    string line;
+    while(getline(cin, line)){
+        if(!line.empty()){
+            cout << line << " is not empty!" << endl;
+        }
+        else {
+            cout << "is empty yo " << line << endl;
+        }
+
+    }
+    return 0;
+} */
+
+
+/* int main(){
+    string line;
+    while(getline(cin, line)){
+        if (line.size() > 80){
+            cout << "The length of " << line << " is greater than 80!" << endl;
+        }
+        else {
+            cout << "The length of " << line << " is not greater than 80!" << endl;
+        }
+    }
+    return 0;
+} */
+
